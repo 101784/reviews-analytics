62df5444eebc1101ab9ad444ef5220ceaa0c1073
Peter <ccc101784@yahoo.com.tw>
2018-12-18 11:04:19 +0800

    add filter

--- read.py
+++ read.py
@@ -11,3 +11,18 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言平均長度為',sum_len / len(data))
+
+new = []
+fff = []
+sss = []
+for d in data:
+	if len(d) < 100:
+		new.append(d) 
+	elif len(d) > 100 < 200:
+		fff.append(d)
+	else:
+		sss.append(d)
+print('總共有', len(new), '筆留言小於100')
+print('總共有', len(fff), '筆留言大於100,小於200')
+print('總共有', len(sss), '筆留言大於200')
+print(new[2])
